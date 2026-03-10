31ac94c719ef061acfc445625cecc72be0391bd4
anthonydenis2005-svg <anthonydenis2005@gmail.com>
2026-03-10 16:14:11 -0400

    Fixed branch to work off local sql for now

--- src/main/java/com/geektext/model/Book.java
+++ src/main/java/com/geektext/model/Book.java
@@ -1,6 +1,11 @@
 package com.geektext.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
     private Long id;
     private String title;
     private String author;
@@ -9,6 +14,7 @@ public class Book {
 
     public Book() {
     }
+
     public Book(Long id, String title, String author, String genre, boolean available) {
         this.id = id;
         this.title = title;
@@ -16,33 +22,43 @@ public class Book {
         this.genre = genre;
         this.available = available;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
+
     public String getGenre() {
         return genre;
     }
+
     public void setGenre(String genre) {
         this.genre = genre;
     }
+
     public boolean isAvailable() {
         return available;
     }
+
     public void setAvailable(boolean available) {
         this.available = available;
     }
