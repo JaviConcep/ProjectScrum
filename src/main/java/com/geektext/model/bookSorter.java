a2a7fc61b8e60ca1bd3f097fcf1f2ced31e7658d
FISWORK <114453461+FISWORK@users.noreply.github.com>
2026-03-24 17:36:37 -0400

    Update bookSorter.java

--- src/main/java/com/geektext/model/bookSorter.java
+++ src/main/java/com/geektext/model/bookSorter.java
@@ -2,24 +2,37 @@ package com.geektext.model;
 
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "bookSorter")
 public class bookSorter{
-
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long bookId;
 
+  @Column(name = "title", nullable = false)
   private String title;
-
+  
+  @Column(name = "author", nullable = false)
   private String author;
-
+  
+  @Column(name = "genre", nullable = false)
   private String genre;
 
+  @Column(name = "price", nullable = false)
   private double price;
-
+  
+  @Column(name = "rating", nullable = false)
   private double rating;
 
+  @Column(name = "sales", nullable = false)
   private int sales;
-
+  
+  @Column(name = "publisher", nullable = false)
   private String publisher;
 
+  public bookSort(){}
+  
   public bookSort(Long bookId, String title, String author, String genre, double price, double rating, int sales, String publisher){
     this.bookId = bookId;
     this.title = title;
