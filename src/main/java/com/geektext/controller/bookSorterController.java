ee405b329a0478ac149b04d9573d33e3480ae3d1
FISWORK <114453461+FISWORK@users.noreply.github.com>
2026-03-24 15:37:18 -0400

    Update bookSorterController.java

--- src/main/java/com/geektext/controller/bookSorterController.java
+++ src/main/java/com/geektext/controller/bookSorterController.java
@@ -1,10 +1,25 @@
 
 package com.geektext.controller;
 
+import com.geektext.model.bookSorter;
+import com.geektext.repository.bookSorterRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+
+@RestController
 
 public class bookSorterController{
 
+  private final bookSorterRepository repository;
 
+  public bookSorterController(bookSorterRepository repository){
+    this.repository = repository;
+  }
+
+  
+  public BookSorter getBooksByGenre(){
+    
+  }
   
 }
