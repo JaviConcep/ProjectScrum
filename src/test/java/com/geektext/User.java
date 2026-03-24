f71570266c7db8b8b480eb6806086721b078a33a
JGit722 <159688403+JGit722@users.noreply.github.com>
2026-03-24 18:07:39 -0400

    Add files via upload

---
+++ src/test/java/com/geektext/User.java
@@ -0,0 +1,42 @@
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private String username;
+
+    private String password;
+    private String name;
+    private String email;
+    private String homeAddress;
+
+    public User() {}
+
+    public User(String username, String password, String name, String email, String homeAddress) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.homeAddress = homeAddress;
+    }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getHomeAddress() { return homeAddress; }
+    public void setHomeAddress(String homeAddress) { this.homeAddress = homeAddress; }
+}
