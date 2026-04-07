053ff1d9b71765553e966790aca2653b74f154df
anthonydenis2005-svg <anthonydenis2005@gmail.com>
2026-04-06 22:20:50 -0400

    Sprint5 Bookdetails

--- src/main/java/com/geektext/service/BookService.java
+++ src/main/java/com/geektext/service/BookService.java
@@ -5,11 +5,17 @@ import com.geektext.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 
     public Book getBookById(Long bookId) {
         return bookRepository.findById(bookId)
