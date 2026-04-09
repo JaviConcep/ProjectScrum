19417c7f23265cc96993a3895829970dc1fa5bc3
FISWORK <114453461+FISWORK@users.noreply.github.com>
2026-04-09 16:10:13 -0400

    Update bookSorterRepository.java

--- src/main/java/com/geektext/repository/bookSorterRepository.java
+++ src/main/java/com/geektext/repository/bookSorterRepository.java
@@ -1,10 +1,16 @@
 package com.geektext.repository;
+
 import com.geektext.model.bookSorter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface bookSorterRepository extends JpaRepository<bookSorter, Long> {
 
-  List<bookSorter> findByGenre(String genre);
-  
+    List<bookSorter> findByGenre(String genre);
+
+    List<bookSorter> findTop10ByOrderBySalesDesc();
+
+    List<bookSorter> findByRatingGreaterThanEqual(double rating);
+
+    List<bookSorter> findByPublisher(String publisher);
 }
